92ff2868f2d1353ee0376cfeddb8d870cfb058a2
caemraul-creator <caemraul@gmail.com>
2026-05-13 14:18:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// =====================================================
+//  KONFIGURASI STOKBAHAN
+//  Ganti nilai di bawah dengan URL Apps Script Anda
+// =====================================================
+
+const CONFIG = {
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyQw_REdtrBJvQSItskyaEfhx5ADbw7yAdEf_o2WJ0r1LSO6qT8PsuJh9dwgqjVr8lQ/exec',
+}
